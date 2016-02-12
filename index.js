a6cc867a8c6d810a137743243da0aff921436530
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2016-02-12 18:34:57 +0100

    initial code

---
+++ index.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const imageType = require('image-type');
+const fs = require('fs');
+const jpeg = require('jpeg-js');
+const PNG = require('png-js');
+const blockhash = require('blockhash');
+
+function hash(filepath, bits, format) {
+  format = format || 'hex';
+  if (format !== 'hex' && format !== 'binary') throw new Error('Unsupported format');
+
+  bits = bits || 8;
+  if (bits % 4 !== 0) throw new Error('Invalid bitlength');
+
+  return new Promise((resolve, reject) => {
+
+    fs.readFile(filepath, (err, content) => {
+      if (err) return reject(err);
+      resolve(content);
+    });
+  })
+  .then((fdata) => {
+    const ftype = imageType(fdata);
+
+    if (ftype.mime === 'image/jpeg') {
+      return jpeg.decode(fdata);
+    }
+
+    if (ftype.mime === 'image/png') {
+      return new Promise((resolve, reject) => {
+        let png = new PNG(fdata);
+        png.decode((pixels) => {
+          resolve({
+            width: png.width,
+            height: png.height,
+            data: pixels
+          });
+        });
+      });
+    }
+
+    throw new Error('Unsupported image type');
+  })
+  .then((data) => hashRaw(data, bits))
+  .then((hexHash) => {
+    if (format === 'hex') return hexHash;
+    if (format === 'binary') return hexToBinary(hexHash);
+  });
+}
+
+function hashRaw(data, bits) {
+  return blockhash.blockhashData(data, bits, 2);
+}
+
+function hexToBinary(s) {
+  const lookup = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'a': '1010',
+    'b': '1011',
+    'c': '1100',
+    'd': '1101',
+    'e': '1110',
+    'f': '1111',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+  };
+  let ret = '';
+  for (let i = 0; i < s.length; i++) {
+    ret += lookup[s[i]];
+  }
+  return ret;
+}
+
+function binaryToHex(s) {
+  const lookup = {
+    '0000': '0',
+    '0001': '1',
+    '0010': '2',
+    '0011': '3',
+    '0100': '4',
+    '0101': '5',
+    '0110': '6',
+    '0111': '7',
+    '1000': '8',
+    '1001': '9',
+    '1010': 'a',
+    '1011': 'b',
+    '1100': 'c',
+    '1101': 'd',
+    '1110': 'e',
+    '1111': 'f'
+  };
+  let ret = '';
+  for (let i = 0; i < s.length; i += 4) {
+    let chunk = s.slice(i, i + 4);
+    ret += lookup[chunk];
+  }
+  return ret;
+}
+
+module.exports = {
+  hash,
+  hashRaw,
+  hexToBinary,
+  binaryToHex
+};
---
+++ test/test.js
@@ -0,0 +1,92 @@
+const assert = require('chai').assert;
+const imghash = require('../index');
+const fs = require('fs');
+const hdist = require('hamming-distance');
+
+it('should create hash for jpeg', function() {
+  return imghash
+    .hash(__dirname + '/files/absolut1')
+    .then((hash) => {
+      assert.ok(hash);
+    });
+});
+
+it('should create hash for png', function() {
+  return imghash
+    .hash(__dirname + '/files/castle1.png')
+    .then((hash) => {
+      assert.ok(hash);
+    });
+});
+
+it('should create same hashes the same images', function() {
+  const h1 = imghash.hash(__dirname + '/files/castle1.png');
+  const h2 = imghash.hash(__dirname + '/files/castle2.png');
+  return Promise
+    .all([h1, h2])
+    .then((res) => {
+      assert.equal(res[0], res[1]);
+    });
+});
+
+it('should create different hashes different images', function() {
+  const h1 = imghash.hash(__dirname + '/files/castle1.png');
+  const h2 = imghash.hash(__dirname + '/files/absolut1');
+  return Promise
+    .all([h1, h2])
+    .then((res) => {
+      var dist = hdist(res[0], res[1]);
+      assert.isAbove(dist, 20);
+    });
+});
+
+it('should create close hashes similar images', function() {
+  const h1 = imghash.hash(__dirname + '/files/absolut2');
+  const h2 = imghash.hash(__dirname + '/files/absolut1');
+  return Promise
+    .all([h1, h2])
+    .then((res) => {
+      var dist = hdist(res[0], res[1]);
+      assert.isBelow(dist, 20);
+    });
+});
+
+it('should support binary output', function() {
+  const h1 = imghash.hash(__dirname + '/files/absolut1', null, 'hex');
+  const h2 = imghash.hash(__dirname + '/files/absolut1', null, 'binary');
+  return Promise
+    .all([h1, h2])
+    .then((res) => {
+      assert.notEqual(res[0], res[1]);
+    });
+});
+
+it('should support validate output format', function() {
+  assert.throws(() => {
+    imghash.hash(__dirname + '/files/absolut1', null, 'foo');
+  });
+});
+
+it('should support variable bits length', function() {
+  const h1 = imghash.hash(__dirname + '/files/absolut1', 8);
+  const h2 = imghash.hash(__dirname + '/files/absolut1', 16);
+  return Promise
+    .all([h1, h2])
+    .then((res) => {
+      assert.equal(res[0].length * 4, res[1].length);
+    });
+});
+
+it('should validate bit lengths', function() {
+  assert.throws(() => {
+    imghash.hash(__dirname + '/files/absolut1', 10);
+  });
+});
+
+it('should expose hexToBinary', function() {
+  assert.equal(imghash.hexToBinary('83C3D381C38985A5'), '1000001111000011110100111000000111000011100010011000010110100101');
+});
+
+it('should expose binaryToHex', function() {
+  assert.equal(imghash.binaryToHex('1000001111000011110100111000000111000011100010011000010110100101'), '83c3d381c38985a5');
+});
