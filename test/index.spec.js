c07c06f9f046cb518e7e8725a702d170e0ed7afb
Alexander Ivanov <47699394+Saiv46@users.noreply.github.com>
2020-12-30 17:53:02 +0800

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -74,9 +74,9 @@ it("should not throw longjmp error", async () => {
 });
 
 it("should support validate output format", async () => {
-  expect.assertions(1);
   await expect(imghash.hash(__dirname + "/files/absolut1", null, "foo"))
-    .resolves;
+    .resolves
+    .toBeDefined();
 });
 
 it("should support variable bits length", async () => {
@@ -86,8 +86,9 @@ it("should support variable bits length", async () => {
 });
 
 it("should validate bit lengths", async () => {
-  expect.assertions(1);
-  await expect(imghash.hash(__dirname + "/files/absolut1", 10)).rejects;
+  await expect(imghash.hash(__dirname + "/files/absolut1", 10))
+    .rejects
+    .toBeDefined();
 });
 
 it("should expose hexToBinary", () => {
