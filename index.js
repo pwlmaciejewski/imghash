aef1a08843dc94e2deae361a07b83bc9819d015f
jakub-wasik <jakub.wasik@schibsted.com>
2022-11-04 23:40:28 +0100

    timhuff hex-binary patch

--- index.js
+++ index.js
@@ -56,60 +56,17 @@ function hashRaw(data, bits) {
 }
 
 function hexToBinary(s) {
-  const lookup = {
-    0: "0000",
-    1: "0001",
-    2: "0010",
-    3: "0011",
-    4: "0100",
-    5: "0101",
-    6: "0110",
-    7: "0111",
-    8: "1000",
-    9: "1001",
-    a: "1010",
-    b: "1011",
-    c: "1100",
-    d: "1101",
-    e: "1110",
-    f: "1111",
-    A: "1010",
-    B: "1011",
-    C: "1100",
-    D: "1101",
-    E: "1110",
-    F: "1111",
-  };
-  let ret = "";
-  for (let i = 0; i < s.length; i++) {
-    ret += lookup[s[i]];
-  }
-  return ret;
+  return s
+    .split("")
+    .map((hexDigit) => parseInt(hexDigit, 16).toString(2).padStart(4, "0"))
+    .join("");
 }
 
 function binaryToHex(s) {
-  const lookup = {
-    "0000": "0",
-    "0001": "1",
-    "0010": "2",
-    "0011": "3",
-    "0100": "4",
-    "0101": "5",
-    "0110": "6",
-    "0111": "7",
-    1000: "8",
-    1001: "9",
-    1010: "a",
-    1011: "b",
-    1100: "c",
-    1101: "d",
-    1110: "e",
-    1111: "f",
-  };
   let ret = "";
   for (let i = 0; i < s.length; i += 4) {
     let chunk = s.slice(i, i + 4);
-    ret += lookup[chunk];
+    ret += parseInt(chunk, 2).toString(16);
   }
   return ret;
 }
